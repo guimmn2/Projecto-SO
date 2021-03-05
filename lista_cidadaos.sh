bea58854aac38022419d592042069447158b2d32
Guilherme Martins Nunes <a98662@iscte.pt>
2021-03-05 11:40:33 +0000

    avancei lista_cidadaos.sh

--- lista_cidadaos.sh
+++ lista_cidadaos.sh
@@ -3,10 +3,14 @@
 ###############################################################################
 ## ISCTE-IUL: Trabalho prático de Sistemas Operativos
 ##
-## Aluno: Nº:       Nome: 
+## Aluno: Nº:98662       Nome:Guilherme Nunes 
 ## Nome do Módulo: lista_cidadaos.sh
 ## Descrição/Explicação do Módulo: 
-##
+## 
+##	 Para este módulo a solução que encontrei é 
+##	 relativamente simples. Basta utilizar os programas awk e sed para obter as
+##	 informações que desejamos e manipulá-las, respectivamente.
 ##
 ###############################################################################
 
+awk -F '[:|-]' '{ OFS = " :"} { print (NR + 10000) , $2,(2021 - $6), $8, $10 }' listagem.txt

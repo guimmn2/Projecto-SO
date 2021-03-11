bb5a51e5129f4910b28652bafd366bf38d969787
guimmn2 <gmnss@protonmail.com>
2021-03-11 21:18:28 +0000

    made some good advancements, now will cleanup the code

--- agendamento.sh
+++ agendamento.sh
@@ -14,6 +14,9 @@
 #verificar que enfermeiros estão disponíveis
 #corresponder a cada cidadão o enfermeiro da zona respectiva
 
+declare -a enfData #inicializa array enfData para guardar dados pertinentes dos enfermeiros
+declare -a cidData #inicializa array cidData para guardar dados pertinentes dos cidadãos
+
 
 verifDisp() {
 
@@ -25,30 +28,76 @@ verifDisp() {
 
 }
 
-#if verifDisp true ; then
-#  echo 'existem!'
-#else
-#  echo 'n existem'
-#fi
-
-#cria array com dados pertinentes dos enfermeiros
-
-declare -a dados 
 
 getEnfData() {
+  
+  grep '1$' enfermeiros.txt | awk -F ":" ' { OFS = ":" } { print $2, $1, $3 }' > tmp.txt
+
+  i=0
+  
+  IFS=$'\n'
+
+  while read line; do
+    enfData+=("$line")
+    #echo "${enfData[$i]}"
+    i=$(( $i + 1 ))
+  done < tmp.txt
+
+  rm tmp.txt
+    
+}
+
+#teste para função getEnfData
+#getEnfData
+#echo "${#enfData[@]}"
+
+
+getCidData() {
+  
+ awk -F ":" '{ OFS = ":" } { print $2, $1, $4 }' cidadaos.txt > tmp.txt 
 
   i=0
 
-  while read line; do
-    dados+=("$line")
-    echo "este é o $iº enfermeiro -->   "${dados[$i]}""
-    i=$(( $i + 1 ))
-  done < enfermeiros.txt
+  IFS=$'\n'
 
+  while read line; do
+    cidData+=("$line")
+    #echo "${cidData[$i]}"
+  done < tmp.txt
+
+  rm tmp.txt
+    
 }
 
-getEnfData
+#teste para função getCidData
+#getCidData
+#echo "${#cidData[@]}"
 
+getEnfData
+getCidData
+
+nrCid="${#cidData[@]}"
+nrEnf="${#enfData[@]}"
+matchRawData() {
+  for (( i=0; i < $nrCid; i++ )); do
+    for (( j=0; j < $nrEnf ;j++)); do
+
+      cidLocal=$( echo "${cidData[$i]}" | cut -d ':' -f3 )
+      enfLocal=$( echo "${enfData[$j]}" | cut -d ':' -f3 | sed 's/CS//g' )
+
+      if [ $cidLocal = $enfLocal ]; then
+        echo "${enfData[$j]}:${cidData[$i]}"
+      fi
+    done
+  done
+}
+#cidLocal=$( echo "${cidData[0]}" | cut -d ':' -f3  )
+#echo "${cidData[0]}"
+#echo "$cidLocal"
+#echo "$nrEnf"
+#echo "$nrCid"
+
+    
 
 
 

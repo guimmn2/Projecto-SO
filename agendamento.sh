21ebd37c90da3e1534812d943889cda329fdc936
guimmn2 <gmnss@protonmail.com>
2021-03-12 13:06:48 +0000

    criei um script de agendamento muito mais compacto

--- agendamento.sh
+++ agendamento.sh
@@ -1,33 +1,5 @@
 #!/bin/bash
 
-###############################################################################
-## ISCTE-IUL: Trabalho prático de Sistemas Operativos
-##
-## Aluno: Nº:       Nome: 
-## Nome do Módulo: agendamento.sh
-## Descrição/Explicação do Módulo: 
-##  
-##  Neste script criei 3 funções. A primeira e mais importante é a verifDisp que verifica se existe sequer algum enfermeiro disponível.
-##  A segunda e a terceira são idênticas. Servem para guardar num array as informações importantes dos enfermeiros e cidadãos.
-##  Para finalizar a parte de criar o ficheiro não é uma função, mas está contingente na existência de enfermeiros. Se existirem enfermeiros
-##  então as duas funções são chamadas e é criado a partir dos arrays um output bem formatado, que é redireccionado para o ficheiro agendamento.txt.
-##  Caso contrário, as funções de informação não são chamadas, o script dá erro, pois não existem enfermeiros disponíveis.
-##  
-##  Os pormenores técnicos podem ser encontrados ao longo do código nos comentários (isto é, a explicação do funcionamento do script concretamente).
-##
-###############################################################################
-
-#ordenar cidadãos por localidade
-#verificar que enfermeiros estão disponíveis
-#corresponder a cada cidadão o enfermeiro da zona respectiva
-
-declare -a enfData #inicializa array enfData para guardar dados pertinentes dos enfermeiros
-declare -a cidData #inicializa array cidData para guardar dados pertinentes dos cidadãos
-
-
-#usando um grep com o flag -q para suprimir o output
-#verifica se existe alguma linha terminada em 1 (se algum enfermeiro está disponível) 
-#retorna true ou false conforme encontra ou não algum enfermeiro
 
 verifDisp() {
 
@@ -39,84 +11,36 @@ verifDisp() {
 
 }
 
+if verifDisp true; then
 
-#com o uso do grep e awk obtem as informações importantes (id, nome e localidade) dos enfermeiros disponíveis
-#OFS = ":" garante output separado por ":"
+  data=$( date +%Y-%m-%d )
 
-getEnfData() {
-  
-  grep '1$' enfermeiros.txt | awk -F ":" ' { OFS = ":" } { print $2, $1, $3 }' > tmp.txt
-
-  i=0
-  
-  #define do delimitador do read como um newline '\n', de forma a que leia linha a linha o ficheiro
-  IFS=$'\n'
-
-  #lê o ficheiro tmp.txt linha a linha e carrega as informações para o array enfData
-  while read line; do
-    enfData+=("$line")
-    i=$(( $i + 1 ))
-  done < tmp.txt
-
-  #remove o ficheiro tmp.txt que serviu apenas para facilitar o armazenamento de informação no array
-  rm tmp.txt
-    
-}
-
-#esta função é idêntica à anterior, mas aplicada ao ficheiro cidadaos.txt
-#obtem o id nome e localidade
-
-getCidData() {
-  
- awk -F ":" '{ OFS = ":" } { print $2, $1, $4 }' cidadaos.txt > tmp.txt 
-
-  i=0
+  awk -F ":" '{ OFS = ":" } { print $2, $1, $4 }' cidadaos.txt > tmp.txt 
 
   IFS=$'\n'
 
   while read line; do
-    cidData+=("$line")
+    localCid=$( echo "$line" | grep -o -E "([A-Za-z])+$" )
+    enfData=$( grep '1$' enfermeiros.txt | grep -i "$localCid" | awk -F ":" '{ OFS = ":"} { print $2, $1, $3 }' )
+    echo "$enfData:$line:$data" | sed -e "s/CS$localCid:// ; s/$localCid/CS$localCid/" >> tmp2.txt
   done < tmp.txt
 
-  rm tmp.txt
-    
-}
 
+  nrLines=$( cat tmp.txt | wc -l ) #para saber quantas linhas ficaram
 
-#funções só são usadas se houver pelo menos 1 enfermeiro disponível
-#se não houver nenhum enfermeiro disponível não vale a pena preencher os arrays com os dados, pois nenhuma vacinação será agendada
+  while read line; do
+    if [[ ! $line =~ ^: ]]; then
+      echo "$line" >> tmp.txt
+    fi 
+  done < tmp2.txt
 
-if verifDisp true; then 
+  tail -n +$(( $nrLines + 1 )) tmp.txt > agendamento.txt
+  rm tmp2.txt tmp.txt
+  exit 0
 
-getEnfData                  #invocação das funções para preencher os arrays devidamente
-getCidData      
-
-nrCid="${#cidData[@]}"      #nr de cidadãos retirado do length do array cidData
-nrEnf="${#enfData[@]}"      #nr de enfermeiros disponiveis retirado do length do array enfData
-data=$(date +%y-%m-%d)      #guarda a data 
-
-  for (( i=0; i < $nrCid; i++ )); do      #a cada cidadão no array cidData
-    for (( j=0; j < $nrEnf ;j++)); do     #por cada cidadão verifica o array enfData
-
-      cidLocal=$( echo "${cidData[$i]}" | cut -d ':' -f3 )                      #para obter a localização de cada cidadadão (na 3ª coluna por ':')
-      enfLocal=$( echo "${enfData[$j]}" | cut -d ':' -f3 | sed 's/CS//g' )      #para obter a localização de cada enfermeiro (na 3ª coluna por ':')
-                                                                                #retira o CS com sed para comparar facilmente as localidades
-
-      if [ $cidLocal = $enfLocal ]; then      #se as localidades forem iguais junta com o formato certo o cidadão com o enfermeiro respectivo      
-
-        #echo + awk para formatar correctamente output para um ficheiro temporário
-        #"CS"$6 adiciona o CS à localidade e o sed -e permite regex adiciona no fim ($) a variável $data
-        #OFS = ":" garante output separado por ":"
-
-        echo "${enfData[$j]}:${cidData[$i]}" | awk -F ":" '{ OFS = ":" } { print $1, $2, $4, $5, "CS"$6 }' | sed -e "s/$/:$data/g" >> tmp.txt
-
-      fi
-    done
-  done
-cat tmp.txt > agendamento.txt       #copia o conteudo de tmp.txt para agendamento.txt, escrevendo por cima
-rm tmp.txt                          
 else
-  echo "nenhum enfermeiro disponível!"
-  echo "sem vacinas hoje!" > agendamento.txt
+  echo "não existem enfermeiros disponíveis!"
+  echo "sem vacinações hoje!" > agendamento.txt
   exit 1
 fi
+

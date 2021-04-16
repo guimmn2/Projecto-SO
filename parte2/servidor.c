b145219aa5c4b3faa23ace449347099bffa8988d
guimmn2 <gmnss@protonmail.com>
2021-04-16 11:51:44 +0100

    cidadao roughed out

--- parte2/servidor.c
+++ parte2/servidor.c
@@ -1,7 +1,7 @@
 /******************************************************************************
  ** ISCTE-IUL: Trabalho prático de Sistemas Operativos
  **
- ** Aluno: Nº:       Nome: 
+ ** Aluno: Nº:98662       Nome:Guilherme Nunes 
  ** Nome do Módulo: servidor.c
  ** Descrição/Explicação do Módulo: 
  **
